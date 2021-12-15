f97509b95868d5db5f13d1f1b90648462a189918
starodubov <denis.starodubov@indriver.com>
2021-12-15 17:02:49 +0300

    Plugin version downgrade

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ gradlePlugin {
         create("BuildManager") {
             id = "com.indriver.laniakea.plugin"
             implementationClass = "LaniakeaPlugin"
-            version = "1.0.0"
+            version = "0.0.1"
         }
     }
 }
