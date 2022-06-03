adfadd30717bdc7b1c9b8e205a885c916f9ba6e4
Denis Verentsov <decsent@yandex.ru>
2022-06-03 21:00:50 +0300

    Upgraded example and added new readme info (#18)
    * Fixed compile and target sdk versions.
    
    * Added few empty files.
    
    * Added ability to connect a debug build.
    
    * Added future plans info in readme.

--- example/features/feature2/src/main/java/com/dvinc/feature_2/Feature2.kt
+++ example/features/feature2/src/main/java/com/dvinc/feature_2/Feature2.kt
@@ -1,3 +1,12 @@
 package com.dvinc.feature_2
 
-object Feature2
+import com.dvinc.feature2_1.Feature21
+import com.dvinc.feature2_2.Feature22
+
+object Feature2 {
+
+    init {
+        Feature21()
+        Feature22()
+    }
+}
